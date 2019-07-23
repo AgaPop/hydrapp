58dc523141f079df7d8ad4dbc6dbf8b7bc1907fe
Aga Popko <aga.majchrzak7@gmail.com>
2019-07-23 19:06:48 +0200

    feat: evenlisteners

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { NPN_ENABLED } from "constants";
+
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
@@ -16,7 +18,33 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const counter = document.querySelector('.counter__value--js');
+const buttonAdd= document.querySelector('.button-add--js');
+const buttonDelete = document.querySelector('.button-delete--js');
+// klucz z datą np: 2019-07-23:
+const key = new Date().toISOString().slice(0,10); 
 
-console.log(`Hello world!`);
+
+if (localStorage.getItem(key)){
+  counter.innerHTML = localStorage.getItem(key);
+} else {
+  localStorage.setItem(key, 0);
+  counter.innerHTML = 0;
+}
+
+buttonAdd.addEventListener('click', (e) => {
+  localStorage.setItem(key, parseInt(localStorage.getItem(key), 10) + 1);
+  counter.innerHTML = localStorage.getItem(key);
+  
+});
+
+buttonDelete.addEventListener('click', (e) => {
+  const currentValue = parseInt(localStorage.getItem(key), 10);
+  if (currentValue > 0) {   
+    localStorage.setItem(key, parseInt(localStorage.getItem(key), 10) -1);    
+    counter.innerHTML = localStorage.getItem(key);
+    
+  }
+});
 
 
